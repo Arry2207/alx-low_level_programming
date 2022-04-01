e276f682fe99a6edc74d321f9c62f134d985510c
Arry2207 <alamin.sansa@gmail.com>
2022-04-01 18:24:04 +0100

    Write a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char* argv[]);
+int main(int argc, char __attribute__((__unused__))*argv[]);
+int atoi(const char *str);
+int _putchar(char c);
+int isdigit( int arg);
+int main(int ac, char *av[]);
+
+#endif
