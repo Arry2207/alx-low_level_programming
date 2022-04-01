fa43a233a604c8736cf45680fbbe341a1656de8f
Arry2207 <alamin.sansa@gmail.com>
2022-04-01 18:33:41 +0100

    Write a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to add command line arguments
+ *
+ * @ac: number of arguments
+ *
+ * @av: array of pointers to a string
+ *
+ * Return: 0 if success
+ *         1 if argument is not integer
+ */
+
+int main(int ac, char *av[])
+{
+	int i, j, k;
+	int sum = 0;
+	char *s;
+
+	for (i = 1; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] < 48 || s[j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		j++;
+		}
+	k = atoi(av[i]);
+	sum += k;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
